ba72ce1d48d2f07d9ccd8d0c9caea24cf3b6e2f1
gretakasongo <gretakasongo07@gmail.com>
2026-07-23 16:15:17 +0200

    style and image

--- firstapp/App.tsx
+++ firstapp/App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View,TextInput, Button, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.welcomeTxt}>
-      <Text>Welcome to my app!</Text>
-      <Text>Enter your name</Text>
-      <TextInput placeholder="Greta" />
-      <Text>Enter your surname</Text>
-      <TextInput placeholder="Kasongo" />
+    <View>
+      <Image source={require("./images/VSCode.png")} />
+      <Text style={styles.welcomeTxt}> Welcome to my app!</Text>
+      <Text style={styles.headingTxt}>Enter your name</Text>
+      <TextInput style={styles.inputBoxTxt} placeholder="Greta" />
+      <Text style={styles.headingTxt}>Enter your surname</Text>
+      <TextInput style={styles.inputBoxTxt} placeholder="Kasongo" />
       <Button title="Add User" />
       <StatusBar style="auto" />
     </View>
@@ -17,11 +18,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   welcomeTxt: {
-    paddingTop: 50, 
+    paddingTop: 50,
     color: "purple",
     fontWeight: "bold",
     fontSize: 30,
     textAlign: "center",
-
   },
+  headingTxt: {
+    fontWeight: "bold",
+  },
+  inputBoxTxt: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    margin: 10,
+  }
+  
+ 
 });
